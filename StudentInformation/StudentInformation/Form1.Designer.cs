a8d63be10072280a2d5d1d5d64c4af3dcaeb1b92
spark01 <btapas28@gmail.com>
2019-09-20 17:23:23 +0600

    Assignmant:4_studentInformation

---
+++ StudentInformation/StudentInformation/Form1.Designer.cs
@@ -0,0 +1,405 @@
+﻿namespace StudentInformation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.idTextBox = new System.Windows.Forms.TextBox();
+            this.gpaTextBox = new System.Windows.Forms.TextBox();
+            this.addresTextBox = new System.Windows.Forms.TextBox();
+            this.ageTextBox = new System.Windows.Forms.TextBox();
+            this.mobileTextBox = new System.Windows.Forms.TextBox();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.ShowButton = new System.Windows.Forms.Button();
+            this.addButton = new System.Windows.Forms.Button();
+            this.showRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.idRadioButton = new System.Windows.Forms.RadioButton();
+            this.nameRadioButton = new System.Windows.Forms.RadioButton();
+            this.mobileRadioButton = new System.Windows.Forms.RadioButton();
+            this.maxTextBox = new System.Windows.Forms.TextBox();
+            this.mimTextBox = new System.Windows.Forms.TextBox();
+            this.nameMaxTextBox = new System.Windows.Forms.TextBox();
+            this.mimNameTextBox = new System.Windows.Forms.TextBox();
+            this.tatalTextBox = new System.Windows.Forms.TextBox();
+            this.averegeTextBox = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // idTextBox
+            // 
+            this.idTextBox.Location = new System.Drawing.Point(91, 31);
+            this.idTextBox.Name = "idTextBox";
+            this.idTextBox.Size = new System.Drawing.Size(100, 20);
+            this.idTextBox.TabIndex = 0;
+            // 
+            // gpaTextBox
+            // 
+            this.gpaTextBox.Location = new System.Drawing.Point(91, 167);
+            this.gpaTextBox.Name = "gpaTextBox";
+            this.gpaTextBox.Size = new System.Drawing.Size(100, 20);
+            this.gpaTextBox.TabIndex = 1;
+            // 
+            // addresTextBox
+            // 
+            this.addresTextBox.Location = new System.Drawing.Point(91, 139);
+            this.addresTextBox.Name = "addresTextBox";
+            this.addresTextBox.Size = new System.Drawing.Size(100, 20);
+            this.addresTextBox.TabIndex = 2;
+            // 
+            // ageTextBox
+            // 
+            this.ageTextBox.Location = new System.Drawing.Point(91, 112);
+            this.ageTextBox.Name = "ageTextBox";
+            this.ageTextBox.Size = new System.Drawing.Size(100, 20);
+            this.ageTextBox.TabIndex = 3;
+            // 
+            // mobileTextBox
+            // 
+            this.mobileTextBox.Location = new System.Drawing.Point(91, 85);
+            this.mobileTextBox.Name = "mobileTextBox";
+            this.mobileTextBox.Size = new System.Drawing.Size(100, 20);
+            this.mobileTextBox.TabIndex = 4;
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(91, 59);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.nameTextBox.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(66, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Id";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(47, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(41, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Mobile:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(56, 114);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Age";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(37, 142);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(45, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Address";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(29, 170);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Gpa point";
+            // 
+            // ShowButton
+            // 
+            this.ShowButton.Location = new System.Drawing.Point(567, 57);
+            this.ShowButton.Name = "ShowButton";
+            this.ShowButton.Size = new System.Drawing.Size(75, 23);
+            this.ShowButton.TabIndex = 12;
+            this.ShowButton.Text = "AllInfo";
+            this.ShowButton.UseVisualStyleBackColor = true;
+            this.ShowButton.Click += new System.EventHandler(this.ShowButton_Click);
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(567, 28);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.TabIndex = 13;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // showRichTextBox
+            // 
+            this.showRichTextBox.Location = new System.Drawing.Point(262, 31);
+            this.showRichTextBox.Name = "showRichTextBox";
+            this.showRichTextBox.Size = new System.Drawing.Size(294, 291);
+            this.showRichTextBox.TabIndex = 14;
+            this.showRichTextBox.Text = "";
+            // 
+            // idRadioButton
+            // 
+            this.idRadioButton.AutoSize = true;
+            this.idRadioButton.Location = new System.Drawing.Point(584, 162);
+            this.idRadioButton.Name = "idRadioButton";
+            this.idRadioButton.Size = new System.Drawing.Size(34, 17);
+            this.idRadioButton.TabIndex = 15;
+            this.idRadioButton.TabStop = true;
+            this.idRadioButton.Text = "Id";
+            this.idRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // nameRadioButton
+            // 
+            this.nameRadioButton.AutoSize = true;
+            this.nameRadioButton.Location = new System.Drawing.Point(584, 185);
+            this.nameRadioButton.Name = "nameRadioButton";
+            this.nameRadioButton.Size = new System.Drawing.Size(53, 17);
+            this.nameRadioButton.TabIndex = 16;
+            this.nameRadioButton.TabStop = true;
+            this.nameRadioButton.Text = "Name";
+            this.nameRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // mobileRadioButton
+            // 
+            this.mobileRadioButton.AutoSize = true;
+            this.mobileRadioButton.Location = new System.Drawing.Point(584, 208);
+            this.mobileRadioButton.Name = "mobileRadioButton";
+            this.mobileRadioButton.Size = new System.Drawing.Size(56, 17);
+            this.mobileRadioButton.TabIndex = 17;
+            this.mobileRadioButton.TabStop = true;
+            this.mobileRadioButton.Text = "Mobile";
+            this.mobileRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // maxTextBox
+            // 
+            this.maxTextBox.Location = new System.Drawing.Point(247, 378);
+            this.maxTextBox.Name = "maxTextBox";
+            this.maxTextBox.Size = new System.Drawing.Size(100, 20);
+            this.maxTextBox.TabIndex = 23;
+            // 
+            // mimTextBox
+            // 
+            this.mimTextBox.Location = new System.Drawing.Point(398, 378);
+            this.mimTextBox.Name = "mimTextBox";
+            this.mimTextBox.Size = new System.Drawing.Size(100, 20);
+            this.mimTextBox.TabIndex = 22;
+            // 
+            // nameMaxTextBox
+            // 
+            this.nameMaxTextBox.Location = new System.Drawing.Point(247, 403);
+            this.nameMaxTextBox.Name = "nameMaxTextBox";
+            this.nameMaxTextBox.Size = new System.Drawing.Size(100, 20);
+            this.nameMaxTextBox.TabIndex = 21;
+            // 
+            // mimNameTextBox
+            // 
+            this.mimNameTextBox.Location = new System.Drawing.Point(398, 409);
+            this.mimNameTextBox.Name = "mimNameTextBox";
+            this.mimNameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.mimNameTextBox.TabIndex = 20;
+            // 
+            // tatalTextBox
+            // 
+            this.tatalTextBox.Location = new System.Drawing.Point(562, 405);
+            this.tatalTextBox.Name = "tatalTextBox";
+            this.tatalTextBox.Size = new System.Drawing.Size(100, 20);
+            this.tatalTextBox.TabIndex = 19;
+            // 
+            // averegeTextBox
+            // 
+            this.averegeTextBox.Location = new System.Drawing.Point(562, 379);
+            this.averegeTextBox.Name = "averegeTextBox";
+            this.averegeTextBox.Size = new System.Drawing.Size(100, 20);
+            this.averegeTextBox.TabIndex = 18;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(522, 407);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(31, 13);
+            this.label7.TabIndex = 29;
+            this.label7.Text = "Total";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(506, 381);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(47, 13);
+            this.label8.TabIndex = 28;
+            this.label8.Text = "Avarage";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(360, 408);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(35, 13);
+            this.label9.TabIndex = 27;
+            this.label9.Text = "Name";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(369, 381);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(26, 13);
+            this.label10.TabIndex = 26;
+            this.label10.Text = "Mim";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(203, 405);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(35, 13);
+            this.label11.TabIndex = 25;
+            this.label11.Text = "Name";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(190, 380);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(48, 13);
+            this.label12.TabIndex = 24;
+            this.label12.Text = "Max gpa";
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(574, 238);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(75, 23);
+            this.searchButton.TabIndex = 30;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 533);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.maxTextBox);
+            this.Controls.Add(this.mimTextBox);
+            this.Controls.Add(this.nameMaxTextBox);
+            this.Controls.Add(this.mimNameTextBox);
+            this.Controls.Add(this.tatalTextBox);
+            this.Controls.Add(this.averegeTextBox);
+            this.Controls.Add(this.mobileRadioButton);
+            this.Controls.Add(this.nameRadioButton);
+            this.Controls.Add(this.idRadioButton);
+            this.Controls.Add(this.showRichTextBox);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.ShowButton);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.mobileTextBox);
+            this.Controls.Add(this.ageTextBox);
+            this.Controls.Add(this.addresTextBox);
+            this.Controls.Add(this.gpaTextBox);
+            this.Controls.Add(this.idTextBox);
+            this.Name = "Form1";
+            this.Text = "StudentInformation";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox idTextBox;
+        private System.Windows.Forms.TextBox gpaTextBox;
+        private System.Windows.Forms.TextBox addresTextBox;
+        private System.Windows.Forms.TextBox ageTextBox;
+        private System.Windows.Forms.TextBox mobileTextBox;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button ShowButton;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.RichTextBox showRichTextBox;
+        private System.Windows.Forms.RadioButton idRadioButton;
+        private System.Windows.Forms.RadioButton nameRadioButton;
+        private System.Windows.Forms.RadioButton mobileRadioButton;
+        private System.Windows.Forms.TextBox maxTextBox;
+        private System.Windows.Forms.TextBox mimTextBox;
+        private System.Windows.Forms.TextBox nameMaxTextBox;
+        private System.Windows.Forms.TextBox mimNameTextBox;
+        private System.Windows.Forms.TextBox tatalTextBox;
+        private System.Windows.Forms.TextBox averegeTextBox;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Button searchButton;
+    }
+}
+
---
+++ StudentInformation/StudentInformation/Form1.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentInformation
+{
+    public partial class Form1 : Form
+    {
+        ErrorProvider ep = new ErrorProvider();
+
+        List<string> ids = new List<string>();
+        List<string> names = new List<string>();
+        List<string> mobs = new List<string>();
+        List<string> ages = new List<string>();
+        List<string> address = new List<string>();
+        List<string> gpas = new List<string>();
+
+        string res = "";
+        int er = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            
+            ep.Clear();
+
+            if ((idTextBox.Text.Equals("") || idTextBox.Text.Length != 4))
+            {
+                er++;
+                ep.SetError(idTextBox, "Enter your Id and Id Must be 4 charecter");
+                return;
+
+            }
+
+            else if (checkDuplicateID()) ;
+            else if (nameTextBox.Text.Equals("") || nameTextBox.Text.Length > 30)
+            {
+                er++;
+                ep.SetError(nameTextBox, "Enter your Name and Name Must be 30 charecter");
+                return;
+
+
+            }
+
+            else if (mobileTextBox.Text.Equals("") || mobileTextBox.Text.Length != 11)
+            {
+                er++;
+                ep.SetError(mobileTextBox, "Mobile Must not be Blank and Must Be 11 Digit");
+                return;
+            }
+            else if (checkDuplicateMobile()) ;
+
+            else if (ageTextBox.Text.Equals("") || ageTextBox.Text.Length > 50)
+            {
+                er++;
+                ep.SetError(ageTextBox, "Enter Your ages");
+                return;
+
+
+            }
+            else if (addresTextBox.Text.Equals(""))
+            {
+                er++;
+                ep.SetError(addresTextBox, "Enter Your ages");
+                return;
+
+
+            }
+
+            else if (gpaTextBox.Text.Equals("") || (float.Parse(gpaTextBox.Text) < 0 || float.Parse(gpaTextBox.Text) > 4))
+            {
+                er++;
+                ep.SetError(gpaTextBox, "GPA Must not be Blank and Between 0-4");
+                return;
+            }
+
+
+            ids.Add(idTextBox.Text);
+            names.Add(nameTextBox.Text);
+            mobs.Add(mobileTextBox.Text);
+            ages.Add(ageTextBox.Text);
+            address.Add(addresTextBox.Text);
+            gpas.Add(gpaTextBox.Text);
+
+            showRichTextBox.Text = "ID: " + idTextBox.Text + "\n" + "Name: " + nameTextBox.Text + "\n" + "Mobile: " + mobileTextBox.Text + "\n"
+                                  + "Age: " + ageTextBox.Text + "\n" + "Address: " + addresTextBox.Text + "\n" + "GPA: " + gpaTextBox.Text;
+
+
+
+            MessageBox.Show("data is added");
+
+            Clear();
+
+
+        }
+
+        private void ShowButton_Click(object sender, EventArgs e)
+        {
+            String minName = "", maxName = "";
+            float min = 10, max = 0, avg = 0, total = 0, gpaText;
+            showRichTextBox.Clear();
+
+
+
+            for (int i = 0; i < ids.Count(); i++)
+            {
+
+                gpaText = float.Parse(gpas[i]);
+
+                total += gpaText;
+
+                if (min > gpaText)
+                {
+                    min = gpaText;
+                    minName = names[i];
+                }
+
+                if (max < gpaText)
+                {
+                    max = gpaText;
+                    maxName = names[i];
+                }
+
+
+
+
+
+
+
+                res += "ID: " + ids[i] + "\n" + "Name: " + names[i] + "\n" + "Mobile: " + mobs[i] + "\n" + "Age: " + ages[i] + "\n" +
+                       "Address: " + address[i] + "\n" + "GPA: " + gpas[i] + "\n" + "\n";
+
+                maxTextBox.Text = max.ToString();
+                nameMaxTextBox.Text = maxName;
+                mimTextBox.Text = min.ToString();
+                mimNameTextBox.Text = minName;
+                tatalTextBox.Text = total.ToString();
+                averegeTextBox.Text = (total / ids.Count()).ToString();
+
+            }
+            //showRichTextBox.Text = "";
+            showRichTextBox.Text = res;
+        }
+
+        private void Clear()
+        {
+            nameTextBox.Text = "";
+            idTextBox.Text = "";
+            mobileTextBox.Text = "";
+            ageTextBox.Text = "";
+            addresTextBox.Text = "";
+            gpaTextBox.Text = "";
+            maxTextBox.Text = "";
+            mimTextBox.Text = "";
+            nameMaxTextBox.Text = "";
+            mimNameTextBox.Text = "";
+            averegeTextBox.Text = "";
+            tatalTextBox.Text = "";
+
+        }
+        private Boolean checkDuplicateID()
+        {
+            for (int j = 0; j < ids.Count(); j++)
+            {
+                if ((idTextBox.Text).Equals(ids[j]))
+                {
+                    er++;
+                    ep.SetError(idTextBox, "iD MUS BE UNIQUE");
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return false;
+        }
+        private Boolean checkDuplicateMobile()
+        {
+            for (int j = 0; j < ids.Count(); j++)
+            {
+                if ((mobileTextBox.Text).Equals(mobs[j]))
+                {
+                    er++;
+                    ep.SetError(mobileTextBox, "This Mobile used Before\nUse an Unique ID");
+               
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return false;
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            if (idRadioButton.Checked == true)
+            {
+                showRichTextBox.Text="";
+                for (int i = 0; i < ids.Count(); i++)
+                {
+                    if (idTextBox.Text.Equals(ids[i]))
+                    {
+                        showRichTextBox.Text = "ID: " + ids[i] + "\n" + "Name: " + names[i] + "\n" + "Mobile: " + mobs[i] + "\n" + "Age: " + ages[i] + "\n" +
+                       "Address: " + address[i] + "\n" + "GPA: " + gpas[i];
+
+                    }
+                    idTextBox.Text="";
+                    Clear();
+                }
+            }
+
+            else if (nameRadioButton.Checked == true)
+            {
+                showRichTextBox.Text="";
+                for (int i = 0; i < names.Count(); i++)
+                {
+                    if (nameTextBox.Text.Equals(names[i]))
+                    {
+                        showRichTextBox.Text = "ID: " + ids[i] + "\n" + "Name: " + names[i] + "\n" + "Mobile: " + mobs[i] + "\n" + "Age: " + ages[i] + "\n" +
+                      "Address: " + address[i] + "\n" + "GPA: " + gpas[i];
+                    }
+                    nameTextBox.Text="";
+                    Clear();
+                }
+            }
+
+            else if (mobileRadioButton.Checked == true)
+            {
+                showRichTextBox.Text="";
+                for (int i = 0; i < mobs.Count(); i++)
+                {
+                    if (mobileTextBox.Text.Equals(mobs[i]))
+                    {
+                        showRichTextBox.Text = "ID: " + ids[i] + "\n" + "Name: " + names[i] + "\n" + "Mobile: " + mobs[i] + "\n" + "Age: " + ages[i] + "\n" +
+                    "Address: " + address[i] + "\n" + "GPA: " + gpas[i];
+
+                    }
+                    mobileTextBox.Text="";
+                    Clear();
+                }
+            }
+        }
+    }
+}
