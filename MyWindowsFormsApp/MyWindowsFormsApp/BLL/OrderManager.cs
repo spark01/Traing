42162862e3357223a9c9efc204808f6c92adb191
spark01 <btapas28@gmail.com>
2019-09-30 16:24:16 +0600

    assignment7

--- MyWindowsFormsApp/MyWindowsFormsApp/BLL/OrderManager.cs
+++ MyWindowsFormsApp/MyWindowsFormsApp/BLL/OrderManager.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿ 
 using System.Data;
-using System.Text;
-using System.Threading.Tasks;
+ 
 using MyWindowsFormsApp.Repository;
 
 namespace MyWindowsFormsApp.BLL
